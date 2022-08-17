931a470e0b222d852c3152ce433b22691371430e
PraveenKumar207 <praveen.alagesan@byjus.com>
2022-08-17 14:48:19 +0530

    Added accuracy percentage with highlights

--- automation_script.rb
+++ automation_script.rb
@@ -6,6 +6,8 @@ require 'open-uri'
 require 'json'
 require 'byebug'
 require 'rubyXL'
+require 'rubyXL/convenience_methods'
+
 
 # Static things that needs to be changed are cohort_id and img_url
 def img_url
@@ -54,24 +56,45 @@ worksheet = workbook[0]
 
 @headers = worksheet[0].cells.map{|c| c.nil? ? nil : c.value}
 
+key_col = @headers.find_index('key')
 img_col = @headers.find_index('preview_image_url')
 cohort_col = @headers.find_index('cohort_id')
-results_found_col = cohort_col + 2
-high_accurate_result_col = cohort_col + 3
-high_accurate_page_col = cohort_col + 4
+result_page_key_col = cohort_col + 2
+results_found_col = cohort_col + 3
+high_accurate_result_col = cohort_col + 4
+high_accurate_page_col = cohort_col + 5
 
+worksheet.add_cell(0, result_page_key_col, 'result_page_key')
 worksheet.add_cell(0, results_found_col, 'results_found?')
 worksheet.add_cell(0, high_accurate_result_col, 'high_accurate_result?')
 worksheet.add_cell(0, high_accurate_page_col, 'high_accurate_page')
 
+
+@last_index = 0
+@match_count = 0
+@total_count = 0
 worksheet.each_with_index do |row, row_index|
-    next if row_index == 0 || worksheet[row_index][img_col].nil? || worksheet[row_index][cohort_col].nil?
+    next if !row || row_index == 0 || worksheet[row_index][img_col].nil? || worksheet[row_index][cohort_col].nil?
     row.cells.each_with_index do |cell, col_index|
         @img_url = cell.value if col_index == img_col
         @cohort_id = cell.value if col_index == cohort_col
     end
     response = get_response
+    # byebug
     puts "\nResponse for row #{row_index} => \n#{response}"
+
+    #result_page_key
+    result_page_key = response['metadata']['pid']
+    worksheet.add_cell(row_index, result_page_key_col, result_page_key)
+    if result_page_key != worksheet[row_index][key_col].value
+        # byebug
+        worksheet.sheet_data[row_index][result_page_key_col].change_fill('ff337e')
+    else
+        @match_count = @match_count + 1
+    end
+    @total_count = @total_count + 1
+
+    #results_found
     results = response['results']
     worksheet.add_cell(row_index, results_found_col, (!results.empty?).to_s)
 
@@ -80,14 +103,20 @@ worksheet.each_with_index do |row, row_index|
     worksheet.add_cell(row_index, high_accurate_result_col, high_accurate_result_found.to_s)
 
     #high_accurate_page
-    high_accurate_page = response['metadata']['accurate']['pid'] rescue 'Not Found'
+    high_accurate_page = response['metadata']['accurate']['pid'] || 'Not Found'
     worksheet.add_cell(row_index, high_accurate_page_col, high_accurate_page)
     
+    @last_index = row_index
 end
+
+accuracy_percent = @match_count.to_f*100/@total_count.to_f
+
+result_percent_row = @last_index + 2
+result_percent_col = result_page_key_col
+worksheet.add_cell(result_percent_row, result_percent_col, 'accuracy %')
+worksheet.add_cell(result_percent_row, result_percent_col + 1, accuracy_percent)
+
 
 workbook.write(file)
 
 puts "\n -- Completed --" 
-
-
-
